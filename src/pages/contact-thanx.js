f46d93a446e0f9e459584d414b8dd0c7789b8116
mako-tos <makotos98@gmail.com>
2020-05-24 19:22:43 +0900

    fix thanx page layout

--- src/pages/contact-thanx.js
+++ src/pages/contact-thanx.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from "gatsby";
 import { Helmet } from 'react-helmet';
+import Layout from '../components/layout'
 
 const Thanx = () => (
-  <>
+  <Layout>
     <Helmet>
       <title>お問合せ</title>
       <meta name="description" content={"お問合せページ"} />
@@ -13,7 +14,7 @@ const Thanx = () => (
       返信をお待ちくださいますようお願い申し上げます。
     </p>
     <Link to={'/'}>Top</Link>に戻る
-  </>
+  </Layout>
 )
 
 export default Thanx
